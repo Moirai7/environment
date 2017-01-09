33ab9388dc78294928cee31d3db813eb07710e80
“Moirai7git config --global user.email “moirai.zhang@gmail.com <moirai.zhang@gmail.com>
2017-01-09 13:28:18 +0800

    update

---
+++ readme.md
@@ -0,0 +1,9 @@
+1. 去除TE=0的值
+2. NPP：
+![NPP](http://ww3.sinaimg.cn/large/006y8lVajw1fbkazm2x9wj31ik0yck7x.jpg)
+41165行，无空值，中值500左右，基本符合正态分布。
+和TE的关系：
+![NPP&TE]()
+2.
+
+
--- viewData.py
+++ viewData.py
@@ -10,28 +10,36 @@ import folium
 def readCSV(filename,index,header):
 	return pd.read_csv(filename, sep=',', engine='python', header=header, index_col=index)
 
-def showInfo(data):
+def showInfo(data,x,y):
 	data.info()
 	print data.head()
+	print data.iloc[:,x]
+	print data.iloc[:,y]
 	pass
 
 def pairedGraph(data,x,y):
 	_fig,_axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 9))
 
-	h = data.iloc[y]
-	r = data.iloc[x]
+	h = data.iloc[:,y].values
+	r = data.iloc[:,x].values
 	colors = np.random.rand(len(r))
-	_axes[0][0].scatter(r, h, s=10, c=colors, alpha=0.5)
-	_axes[0][1].boxplot([h,r],patch_artist=True,vert=True)
-	_axes[0][2].violinplot([h,r])
+	_axes[0][0].scatter(r, h, s=100, c=colors, alpha=0.5)
+	_axes[0][0].set_title('scatter')
+	_axes[0][1].boxplot([r,h],patch_artist=True,vert=True)
+	_axes[0][1].set_title('boxplot')
+	_axes[0][2].violinplot([r,h])
+	_axes[0][2].set_title('violinplot')
 	_axes[1][0].hist([r,h], 20, normed=1, histtype='bar')
-	_axes[1][1].plot(xrange(0,len(h)),h,'-o')
+	_axes[1][0].set_title('hist')
+	_axes[1][1].plot(xrange(0,len(r)),h,'-o')
 	_axes[1][1].plot(xrange(0,len(h)),r,'-x')
+	_axes[1][1].set_title('plot')
 	x = xrange(0,len(h))
 	density = gaussian_kde(h)
         density.covariance_factor = lambda : .25
         density._compute_covariance()
 	_axes[1][2].plot(x,density(x),'-o')
+	_axes[1][2].set_title('density')
 	density = gaussian_kde(r)
 	density.covariance_factor = lambda : .25
 	density._compute_covariance()
@@ -42,23 +50,28 @@ def pairedGraph(data,x,y):
 def oneGraph(data,x):
 	_fig,_axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 9))
 	
-	h = data.iloc[x]
+	h = data.iloc[:,x].values
 	x = xrange(0,len(h))
 	_axes[0][0].boxplot(h,patch_artist=True,vert=True)
+	_axes[0][0].set_title('boxplot')
 	_axes[0][1].violinplot(h)
+	_axes[0][1].set_title('violinplot')
 	stats.probplot(h, dist="norm", plot=_axes[0][2])
 	_axes[1][0].plot(x,h,'-o')
+	_axes[1][0].set_title('plot')
 	density = gaussian_kde(h)
 	density.covariance_factor = lambda : .25
 	density._compute_covariance()
 	_axes[1][1].plot(x,density(x))
+	_axes[1][1].set_title('density')
 	plt.hist(h,bins=20,normed=True)
+	_axes[1][2].set_title('hist')
 	plt.show()
 
 def drawMap(x,y,z):
-	h = data.iloc[y]
-	r = data.iloc[x]
-	z = data.iloc[z]
+	h = data.iloc[:,y].values
+	r = data.iloc[:,x].values
+	z = data.iloc[:,z].values
 	maps = folium.Map(location=[30, 0], zoom_start=2)
 	for a,b,c in zip(r,h,z):
 		print c
@@ -111,8 +124,10 @@ if __name__ == '__main__':
 
 	plt.style.use('ggplot')
 	data = readCSV(filename, index, header)
+	data = data[data.TE!=0]
+
 	if info:
-		showInfo(data)
+		showInfo(data,x,y)
 	if graph:
 		oneGraph(data,x)
 	if paired:
